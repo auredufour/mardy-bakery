5577f954f17cece2fff7d7fc883af4de8ce83ca0
auredufour <adufour@wealthsimple.com>
2021-03-27 15:55:25 -0400

    Fix deploy

---
+++ src/components/main/index.js
@@ -0,0 +1,100 @@
+import React, { useEffect } from "react"
+import { StaticImage } from "gatsby-plugin-image"
+import _get from "lodash/get"
+import Img from "gatsby-image"
+import { useInstagramQuery } from "./api/useAllInstagramContent.js"
+
+import { menu, offers } from "./constants"
+import {
+  Banner,
+  Header,
+  Insta,
+  InstaFeed,
+  InstaImg,
+  ItemSubTitle,
+  ItemTitle,
+  Menu,
+  Offer,
+  OffersContainer,
+  StaticImageContainer,
+  hiddenStyles,
+} from "./styles"
+
+const Item = ({ title, subtitle, content }) => (
+  <li>
+    <ItemTitle>{title}</ItemTitle>
+    {subtitle && <ItemSubTitle>{subtitle}</ItemSubTitle>}
+    {content && (
+      <ul>
+        {content.map(({ name, price, additionalPrice }) => (
+          <ItemSubTitle>{`${name} - ${price}${
+            additionalPrice ? additionalPrice : ""
+          }`}</ItemSubTitle>
+        ))}
+      </ul>
+    )}
+  </li>
+)
+
+const MainPage = () => {
+  const data = useInstagramQuery()
+
+  return (
+    <>
+      <Banner>
+        <StaticImageContainer id="logoImage">
+          <Header level="1" style={{ ...hiddenStyles }}>
+            Mardy Bakery
+          </Header>
+          <StaticImage
+            src="../../images/Image.png"
+            quality={95}
+            formats={["AUTO", "WEBP", "AVIF"]}
+            alt=""
+            style={{ width: "100%" }}
+          />
+        </StaticImageContainer>
+      </Banner>
+      <Offer withBackground>
+        <Header level={2}>WE OFFER</Header>
+        <OffersContainer>
+          {offers.map(offer => (
+            <Item {...offer} />
+          ))}
+        </OffersContainer>
+      </Offer>
+      <Menu withBackground>
+        <Header level={2}>MENU OF THE WEEK</Header>
+        <OffersContainer withMargin>
+          {menu.map(item => (
+            <Item {...item} />
+          ))}
+        </OffersContainer>
+        <ItemSubTitle color="#383838">
+          <span>Order by Friday 7PM by email – </span>
+          <a href="mailto:info@mardybakery.com">info@mardybakery.com</a>
+        </ItemSubTitle>
+        <ItemSubTitle color="#383838">
+          Pay by card or cash on pick up or delivery.
+        </ItemSubTitle>
+      </Menu>
+      <Insta>
+        <Header level={2}>FOLLOW US ON INSTAGRAM</Header>
+        <a href="https://www.instagram.com/mardy.bakery/">
+          <ItemSubTitle color="#383838">@mardy.bakery</ItemSubTitle>
+        </a>
+        <InstaFeed>
+          {data.map((item, i) => {
+            return (
+              <InstaImg key={i}>
+                <Img fixed={item.node.localImage.childImageSharp.fixed} />
+              </InstaImg>
+            )
+          })}
+        </InstaFeed>
+      </Insta>
+    </>
+  )
+}
+
+export default MainPage
---
+++ src/components/main/styles.js
@@ -0,0 +1,121 @@
+import styled, { createGlobalStyle } from "styled-components"
+
+export const GlobalStyles = createGlobalStyle`
+body {
+    font: 400 18px Rubik, sans-serif;
+    background-color: #FFB4AB;
+    color: #D5271A;
+  }
+`
+
+export const SectionContainer = styled.section`
+  width: 100%;
+  padding: 60px;
+  max-width: 800px;
+  margin: 0 auto 40px;
+
+  ${({ withBackground }) => withBackground && `background-color: #ffeeee`};
+`
+
+export const About = styled(SectionContainer)``
+export const Menu = styled(SectionContainer)``
+export const Offer = styled(SectionContainer)``
+
+export const InstaImg = styled.li`
+  display: block;
+  width: 33%;
+  padding: 0;
+  margin: 0;
+
+  img {
+    display: block;
+  }
+`
+
+export const Banner = styled(SectionContainer)``
+
+export const StaticImageContainer = styled.div`
+  margin: 0 auto;
+  text-align: center;
+  width: 50%;
+  transition: width ease 0.2s;
+`
+
+export const OffersContainer = styled.ul`
+  li {
+    border-bottom: 1px solid #d5271a;
+    margin-bottom: 15px;
+    padding-bottom: 20px;
+  }
+
+  li:last-child {
+    border-bottom: 0;
+    margin-bottom: 0;
+    padding-bottom: 0;
+  }
+
+  ${({ withMargin }) => withMargin && `margin-bottom: 40px`};
+`
+
+export const ItemTitle = styled.p`
+  font-size: 14px;
+  font-weight: 700;
+  margin-bottom: 7px;
+`
+
+export const ItemSubTitle = styled.p`
+  font-size: 14px;
+  font-weight: 500;
+  margin-bottom: 7px;
+
+  a {
+    color: inherit;
+  }
+
+  ${({ color }) => color && `color: ${color}`};
+`
+
+export const Header = styled.div.attrs(({ level }) => ({
+  role: "header",
+  "aria-level": level,
+}))`
+  color: #383838;
+  font-size: 24px;
+  letter-spacing: 10%;
+  margin-bottom: 30px;
+`
+
+export const InstaFeed = styled.ul`
+  max-width: 800px;
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0 auto 40px;
+  justify-content: space-between;
+`
+
+export const Insta = styled(SectionContainer)`
+  padding: 60px 0;
+  text-align: center;
+  color: #383838;
+
+  div[role="header"] {
+    margin-bottom: 10px;
+  }
+
+  a {
+    color: inherit;
+    margin-bottom: 80px;
+    font-size: 14px;
+    font-weight: 500;
+    display: block;
+  }
+`
+
+export const hiddenStyles = {
+  position: "absolute",
+  left: "-10000px",
+  top: "auto",
+  width: "1px",
+  height: "1px",
+  overflow: "hidden",
+}
