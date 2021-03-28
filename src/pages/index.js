a5c1d3b40b0586d8073abd66fb711baaaf72de37
auredufour <adufour@wealthsimple.com>
2021-03-28 11:41:07 -0400

    Change main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,19 @@
-import * as React from "react"
-import { StaticImage } from "gatsby-plugin-image"
+import React from "react"
+import _get from "lodash/get"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Main from "../components/main"
+import { GlobalStyles } from "../components/main/styles"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div
-      style={{
-        width: "100%",
-        "background-color": "#FBB4AC",
-        textAlign: "center",
-        "font-family": "Roboto, sans-serif",
-      }}
-    >
-      <StaticImage
-        src="../images/mardy-bakery-logo.png"
-        quality={95}
-        formats={["AUTO", "WEBP", "AVIF"]}
-        alt=""
-        style={{ width: "80%", maxHeight: "100vh" }}
-      />
-    </div>
-  </Layout>
+const TestPage = () => (
+  <>
+    <GlobalStyles />
+    <Layout>
+      <SEO title="Home" />
+      <Main />
+    </Layout>
+  </>
 )
 
-export default IndexPage
+export default TestPage
