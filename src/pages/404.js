fe4564b4ee138ee2a54a2851075e29300adbc7c8
auredufour <adufour@wealthsimple.com>
2022-03-27 17:59:30 -0400

    clean up 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,16 +2,21 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { Home } from "../components/main/v2/pages/home"
 import { GlobalStyles } from "../components/main/v2/styles"
+import {Layout as PageLayout} from '../components/main/v2/components/layout'
+
 
 const NotFoundPage = () => (
-   <>
+  <>
     <GlobalStyles />
     <Layout>
       <SEO title="Home" />
-      <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <PageLayout headerText="About">
+        <div style={{textAlign: 'center'}}>
+          <h1>404: Not Found</h1>
+          <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        </div>
+      </PageLayout>
     </Layout>
   </>
 )
