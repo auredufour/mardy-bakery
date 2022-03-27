887d9c8da7e2e0e3bc8ac54c60703524f83be8e2
auredufour <adufour@wealthsimple.com>
2022-03-27 18:22:24 -0400

    path

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
       options: {
         name: `Mardy Bakery website`,
         short_name: `Mardy Bakery website`,
-        start_url: `/`,
+        start_url: `/home`,
         background_color: `#FFB4AB`,
         theme_color: `#FFB4AB`,
         display: `minimal-ui`,
